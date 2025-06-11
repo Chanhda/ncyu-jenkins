55c0793f7969676f310324e38cafaaff365ee575
Hoc vien <trieuchanhda10c1.2019@gmail.com>
2025-06-11 15:17:05 +0700

    Test code for Jenkins

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class GradeConverterTest {
+    private GradeConverter converter;
+    @Before
+    public void setUp() throws Exception {
+        converter = new GradeConverter();
+    }
+    @Test
+    public void testConvert() {
+        String expected = "A";
+        String actual = converter.convert(100);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testConvert2() {
+        String expected = "A";
+        String actual = converter.convert(90);
+        assertEquals(expected, actual);
+    }
+}
